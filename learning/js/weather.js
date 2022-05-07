ca2e2ff20bc8f88213e0c67fb8536540d6479feb
swamptoday <maryna.nyzova.sa.2021@lpnu.ua>
2022-05-07 17:30:37 +0300

    weather (temp for main page)

---
+++ learning/js/weather.js
@@ -0,0 +1,15 @@
+function weatherBalloon( cityName ) {
+    var key = 'a692c4bc95275382624df8e34d89d462';
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key)  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+      document.getElementById("celc").innerHTML = Math.round((data['main']['temp'])-273.15) + "<sup>&#8451</sup>";
+    })
+    .catch(function() {
+      // catch any errors
+    });
+  }
+  
+  window.onload = function() {
+    weatherBalloon('Lviv');
+  }
