570f52c1fbbc8f930038fb9cb3739cc4e20da27d
swamptoday <maryna.nyzova.sa.2021@lpnu.ua>
2022-05-12 09:44:48 +0300

    page for map with cabinets

---
+++ learning/js/uni-cabinet-maps.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+  direction: 'vertical',
+  slidesPerView: 1,
+  mousewheel: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
