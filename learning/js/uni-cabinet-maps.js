81ed6b6f775459975ddd2184d2a70622d95aa831
swamptoday <maryna.nyzova.sa.2021@lpnu.ua>
2022-05-12 10:25:54 +0300

    right path

--- learning/js/uni-cabinet-maps.js
+++ learning/js/uni-cabinet-maps.js
@@ -7,3 +7,14 @@ var swiper = new Swiper('.swiper-container', {
     clickable: true,
   },
 });
+
+var corpusNumbers = ("Головний корпус","1","2","3","4","5","6","7","8","9","10","11","14","15","21","28");
+
+var innercorpusHTML = "<h6 class='dropdown-header'>Оберіть корпус</h6>";
+var newCorpus;
+
+for(var i=0;i<corpusNumbers.length;i++)
+{
+  var newCorpus = newCorpus + "<a onclick='instituteText(this)' class='dropdown-item' href='#'>"+ corpusNumbers[i] +"</a>";
+}
+console.log(newCorpus);
