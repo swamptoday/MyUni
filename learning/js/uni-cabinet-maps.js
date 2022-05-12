26e094d607939afeed4bcf37c3a49c6d393c4d17
swamptoday <maryna.nyzova.sa.2021@lpnu.ua>
2022-05-12 12:46:18 +0300

    cabinet page update

--- learning/js/uni-cabinet-maps.js
+++ learning/js/uni-cabinet-maps.js
@@ -8,13 +8,24 @@ var swiper = new Swiper('.swiper-container', {
   },
 });
 
-var corpusNumbers = ("Головний корпус","1","2","3","4","5","6","7","8","9","10","11","14","15","21","28");
 
+var corpusNumbers = ["Головний корпус","1","2","3","4","5","6","7","8","9","10","11","14","15","21","28"];
 var innercorpusHTML = "<h6 class='dropdown-header'>Оберіть корпус</h6>";
-var newCorpus;
-
-for(var i=0;i<corpusNumbers.length;i++)
+var newCorpus = "<a id=istituteItem onclick='corpusText(this)' class='dropdown-item' href='#'>"+ corpusNumbers[0] +"</a>";
+for(var i=1; i<corpusNumbers.length; i++)
 {
-  var newCorpus = newCorpus + "<a onclick='instituteText(this)' class='dropdown-item' href='#'>"+ corpusNumbers[i] +"</a>";
+  newCorpus = newCorpus + "<a id=istituteItem onclick='corpusText(this)' class='dropdown-item' href='#'>"+ corpusNumbers[i] +"</a>";
 }
-console.log(newCorpus);
+innercorpusHTML = innercorpusHTML + newCorpus;
+document.getElementById("corpusDropDownitems").innerHTML = innercorpusHTML;
+
+
+function corpusText(element)
+{
+    document.getElementById("corpusButton").innerText = element.innerText;
+}
+
+function changename(element)
+{
+    console.log(element.title);
+}
