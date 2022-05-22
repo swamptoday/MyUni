e57a854902ceeb466316dcdaa374569483c66a44
swamptoday <maryna.nyzova.sa.2021@lpnu.ua>
2022-05-22 17:21:38 +0300

    getting schedule (try2)

--- learning/js/weekschedule.js
+++ learning/js/weekschedule.js
@@ -7,13 +7,32 @@ function instituteText(element)
 {
     document.getElementById("instituteButton").innerText = element.innerText;
 }
+const Monday = document.querySelector('#Monday');
+const Tuesday = document.querySelector('#Tuesday');
+const Wednesday = document.querySelector('#Wednesday');
+const Thursday = document.querySelector('#Thursday');
+const Friday = document.querySelector('#Friday');
+var i = 0;
+var j = 0;
+var parttext;
 
 const logFileText = async file => {
     const response = await fetch(file);
     const text = await response.text();
-    console.log(text);
-    alert(text[1]);
+    
+    for (i; i<40;i++)
+    {
+        parttext[i] = text[i];
+    }
+    console.log(parttext);
+    
 }
+
 
 logFileText('АВ-11.txt');
 //readTextFile("file:///C:/MyData/DesktopУнивер/MyUni Project/MyUni/MyUni/schedule-txts");АВ-11.txt
+
+function getClassNum(text)
+{
+
+}
